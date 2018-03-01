f898b5d4e03e1b32402286737af7c0791b891643
Leo Przybylski <r351574nc3@gmail.com>
2018-02-28 21:57:38 -0700

    Removing WIF. Adding quick verification of return type from SC2. Updated model and migrations for refresh token.
    Implemented refresh token instead of wif for voting.
    
    Including fix for sending incorrect messaging to non-resisters

--- migrations/20180214004447-create-preferences.js
+++ migrations/20180214004447-create-preferences.js
@@ -31,7 +31,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    })
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('Preferences');
