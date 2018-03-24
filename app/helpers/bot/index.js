05aaef967fe22b375c22a34d94df8d0c0707ebd8
Leo (중광) Przybylski <r351574nc3@gmail.com>
2018-03-23 19:29:34 -0700

    Revert "Voting and comment queues"

--- app/helpers/bot/index.js
+++ app/helpers/bot/index.js
@@ -1,36 +1,12 @@
 'use strict'
 
+const scheduler = require('node-schedule')
+
 
 module.exports = {
     run
 }
 
-const voting_queue = [];
-const comment_queue = [];
-
-const voting = {
-    length: () => { return voting_queue.length },
-    push: (obj) => { return voting_queue.push(obj) },
-    pop: () => { return voting_queue.pop() },
-    shift: () => { return voting_queue.shift() },
-    unshift: (obj) => { return voting_queue.unshift(obj) }
-}
-
-const comments = {
-    length: () => { return comment_queue.length },
-    push: (obj) => { 
-        return comment_queue.push(obj) 
-    },
-    pop: () => { return comment_queue.pop() },
-    shift: () => {
-        console.log("Shifting comments ", comment_queue)
-        return comment_queue.shift() 
-    },
-    unshift: (obj) => { return comment_queue.unshift(obj) }
-}
-
 function run() {
-    require('./vote').execute(voting);
-    require('./comment').execute(comments);
-    require('./task').execute(voting, comments);
+    require('./task').execute()
 }
